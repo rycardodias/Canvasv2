93af91bbc9ef1e55d31e37e55adb5a380f3a8fa8
Ricardo Dias <rycardo.dias@hotmail.com>
2022-04-26 18:31:24 +0100

    .

--- scripts/dimensions.js
+++ scripts/dimensions.js
@@ -1,7 +1,7 @@
 let scale = 1
 let initialWidth = window.innerWidth
 
-canvas.width = window.innerWidth * .8
+canvas.width = window.innerWidth * .7
 canvas.height = canvas.width * .5
 
 let globalRadius = canvas.width * .025;
